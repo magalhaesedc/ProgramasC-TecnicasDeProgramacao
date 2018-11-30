d65ab77104095ac193813a9c9484f1d70814a2cd
magalhaesedc <45473112+magalhaesedc@users.noreply.github.com>
2018-11-29 22:02:36 -0300

    Create calcularNaf.c

---
+++ calcularNaf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main(){
+
+    float n1, n2, naf, media;
+
+    printf("Digite a nota do NAP1: ");
+    scanf("%f", &n1);
+
+    printf("Digite a nota do NAP2: ");
+    scanf("%f", &n2);
+
+    printf("Digite a nota do NAF: ");
+    scanf("%f", &naf);
+
+}
